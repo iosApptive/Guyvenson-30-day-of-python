1f7b74bd8ca20b5d0960f9d43d4e4af672501b52
guyvenson cadet <createwidthcodes@gmail.com>
2025-03-09 19:40:01 -0400

    i learn the about operation in python even thought the a few more that i did not list and give examples to.

---
+++ 02_operation.py
@@ -0,0 +1,28 @@
+
+# declaration of a variable and its asigned value which is the number 1
+number_one = 1
+
+# add 2 to variable number_one
+number_one += 6
+print(number_one)
+
+# remove 2 from variable number_one
+number_one -= 2
+print(number_one)
+
+# multiplies variable number_one times 2
+number_one *= 2
+print(number_one)
+
+# divide number number_one by 2 which now equal 10
+number_one /= 2
+print(number_one)
+
+# gets the remainder after dividing number_one by 2
+# can also be use to check if a number is even or prime
+number_one % 2
+print(number_one)
+
+# number_one to the power of 2
+number_one **= 2
+print(number_one)
