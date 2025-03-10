b95586c56a17f36ff00cff9079c792e8217daf53
guyvenson cadet <createwidthcodes@gmail.com>
2025-03-09 20:00:21 -0400

    i learn about data types in python and how they behave.

---
+++ 03_data_type.py
@@ -0,0 +1,27 @@
+
+# a string is define with single or double quotes
+name = "guyvenson"
+
+# intergers are numbers without decimal values
+interger = 5
+my_interger = int(5)
+
+# floats are numbers with decimal place value
+floats = 4.3
+my_float_number = float(4.3)
+
+# a list of numbers or array
+# you can add and remove value within a list
+# you can access specific values by its index which starts at zero
+my_list = [1,5,2,3,6]
+
+# dictionaries are key value pairs 
+# you can also add and remove or change values within a dictionary
+# to access values by providing the key
+my_dict = {"name":"Guyvenson","location":"Cambridge"}
+
+# tuple is a collection of data that cannot be change
+my_tuple = ("apple","pear","dates")
+
+# sets are colection of data that is unordered and connot have duplicates
+my_set = {"banana","kiwi","orange"}
